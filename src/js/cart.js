74f2467ce54818b2e4562d9a731f4c82f45aa2c7
Teresa <teresabroyer2003@gmail.com>
2024-02-11 15:20:48 -0500

    fix copys of products in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -18,6 +18,20 @@ function getCart() {
 document.addEventListener("DOMContentLoaded", e =>{
   addCart()
 })
+function getCart() {
+  let cart = localStorage.getItem("cart-products");
+  if(cart && cart  != "undefined" ){
+    return JSON.parse(cart);
+  }else{
+    localStorage.setItem("cart-products", JSON.stringify([]));
+    return JSON.parse(localStorage.getItem("cart-products"));
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", e =>{
+  addCart()
+})
 document.addEventListener("click", (event) => {
   const clickedElement = event.target;
   if (clickedElement.id === "nav-cart" || clickedElement.id === "cart-close") {
@@ -31,13 +45,17 @@ const container__items = document.querySelector(".container__items");
 
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("options__cart")) {
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("options__cart")) {
     addCart();
+  }
+});
   }
 });
 
 function addCart() {
   let content = "";
-  let total = 0;
   const cartProducts = getCart();
   cartProducts.map(e =>{
     content += `
@@ -45,15 +63,13 @@ function addCart() {
               <img src=${e.images[0]} alt="" class="item__image">
               <div class="item__info">
               <h4 class="item__title">${e.name}</h4>
-              <p class="item__price"><span>${e.product_cart}</span> X <span>Rs.  ${e.price * e.product_cart}</span></p>
+              <p class="item__price"><span>${e.product_cart}</span> X <span>Rs.  ${e.price}</span></p>
               </div>
               <div class="item__delete-container">
               <img src="../src/images/delete.svg" alt="" class="item__delete">
               </div>
               </div>
           `;
-    total += e.price * e.product_cart;
   })
-  totalHtml.textContent = total
   container__items.innerHTML = content;
 }
