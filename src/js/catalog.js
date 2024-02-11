8f70c872ae2e3a042c78290b5be8efa83a8abf15
Teresa <teresabroyer2003@gmail.com>
2024-02-11 15:20:48 -0500

    fix copys of products in cart

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -17,6 +17,25 @@ async function fetchProductData() {
     displayProductsInCatalog();
 };
 
+const cartHtml = document.getElementById("cart");
+
+function toggleCart() {
+  cartHtml.classList.toggle("display--none");
+}
+
+document.addEventListener("DOMContentLoaded", e =>{
+    fetchProductData();
+})
+
+async function fetchProductData() {
+    await fetch("http://127.0.0.1:5500/src/JSON/product.json")
+        .then(res => res.json())
+        .then(data=> localStorage.setItem("products", JSON.stringify(data)))
+        .catch(error => console.log(error));
+    
+    displayProductsInCatalog();
+};
+
 function fetchProducts() {
     let products = JSON.parse(localStorage.getItem("products")).products;
     return products;
@@ -27,14 +46,31 @@ function fetchProduct(id){
     let products = fetchProducts();
     let selectedProduct = products.filter(e => e.id == id);
     localStorage.setItem("selected-product", JSON.stringify(selectedProduct));
-    console.log(selectedProduct);
+    return selectedProduct;
 }
-async function fetchProductData() {
-    await fetch("http://127.0.0.1:5500/boutique_project/src/JSON/product.json")
-        .then(res => res.json())
-        .then(data=> localStorage.setItem("products", JSON.stringify(data)))
-        .catch(error => console.log(error));
-};
+
+function getCart() {
+    const cart = localStorage.getItem("cart-products");
+    return cart ? JSON.parse(cart) : [];
+}
+
+function fetchProductsCart(id) {
+    const product = fetchProduct(id)[0];
+    let cart = getCart();
+
+    const existingProduct = cart.find(e => e.id === product.id);
+
+    if (existingProduct) {
+        existingProduct.product_cart++;
+    } else {
+        product.product_cart = 1;
+        cart.push(product);
+    }
+
+    localStorage.setItem("cart-products", JSON.stringify(cart));
+    toggleCart();
+}
+
 function displayProductsInCatalog(){
     const padreProductos = document.getElementById("padreProductos");
     const products = fetchProducts();
@@ -59,10 +95,18 @@ function displayProductsInCatalog(){
         </div>
         `
     padreProductos.appendChild(card);
+
     const title = document.getElementById(`product-title-${product.id}`);
+    const buttonProduct = document.getElementById(`btn-${product.id}`);
+
+
     title.addEventListener("click",(event)=>{
 
+
         fetchProduct(product.id);
+    } );
+    buttonProduct.addEventListener("click",(event)=>{
+        fetchProductsCart(product.id);
     } );
     buttonProduct.addEventListener("click",(event)=>{
         fetchProductsCart(product.id);
@@ -73,7 +117,5 @@ function displayProductsInCatalog(){
 
 
 
-fetchProductData();
-displayProductsInCatalog();
 
 
