ddb757409571197de1f8859ecca2e803886b5bf7
dpa3006 <dpa3006@gmail.com>
2024-03-04 09:39:46 -0500

    changes in catalog search

--- src/js/catalog_search.js
+++ src/js/catalog_search.js
@@ -1,40 +1,47 @@
-document.addEventListener('DOMContentLoaded', () =>{
-  showResults()
-});
-let result = document.getElementById('search__results');
-function showResults(){
-    catalog.forEach(auto => {
-      const elements = document.createElement("div");
-      elements.innerHTML = 
-      card.classList.add("product--cart")
-      card.innerHTML = `
-      <div id=${product.id} class="product--card">
-          <div class="img__options">
-              <img class="product__img" src=${product.images}>
-              <div class="product__options">
-                  <button  id="btn-${product.id}" class="options__cart">Añadir al carrito</button>
-              </div>
-          </div>
-          <div class="product__text">
-              <a href="../../pages/product.html" id="product-title-${product.id}" class="product__title">
-                  <p class="product__title">${product.name}</p>
-              </a>
-              <p class="product__description">${product.description}</p>
-              <p class="product__price">$${product.price.toLocaleString()}</p>
-          </div>
-      </div>
-      `
-    result.appendChild(elements)
-    });
-}
+// document.addEventListener('DOMContentLoaded', () =>{
+//   showResults();
+//   searchProducts();
+// });
+// let result = document.getElementById('search__results');
+//     search = document.getElementById('search__input')
+//    let products;
+// function showResults(){
+//     products.forEach(products => {
+    
+//       const elements = document.createElement("div");
+//       elements.innerHTML = 
+//       card.classList.add("product--cart")
+//       card.innerHTML = `
+//       <div id=${product.id} class="product--card">
+//           <div class="img__options">
+//               <img class="product__img" src=${product.images}>
+//               <div class="product__options">
+//                   <button  id="btn-${product.id}" class="options__cart">Añadir al carrito</button>
+//               </div>
+//           </div>
+//           <div class="product__text">
+//               <a href="../../pages/product.html" id="product-title-${product.id}" class="product__title">
+//                   <p class="product__title">${product.name}</p>
+//               </a>
+//               <p class="product__description">${product.description}</p>
+//               <p class="product__price">$${product.price.toLocaleString()}</p>
+//           </div>
+//       </div>
+//       `
+//     result.appendChild(elements)
+//     });
+// }
+//     function searchProducts(){
+//         search.addEventListener("input", e => 
+//         console.log("Se está ejecutando"))
+//     }
+// // const searchInput = document.getElementById('search__input');
+// // const searchButton = document.getElementById('button__search');
+// // const searchResults = document.getElementById('search__results');
 
-// const searchInput = document.getElementById('search__input');
-// const searchButton = document.getElementById('button__search');
-// const searchResults = document.getElementById('search__results');
+// // function innerSearch(){
 
-// function innerSearch(){
-
-//   filter = searchInput.value.toUpperCase();
+// //   filter = searchInput.value.toUpperCase();
   
 
-// }
+// // }
