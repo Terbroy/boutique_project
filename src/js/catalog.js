e4e5df37a97881a0ffc633b60e80266d2f23c912
Teresa <teresabroyer2003@gmail.com>
2024-03-06 09:41:34 -0500

    fix filter

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -59,7 +59,8 @@ async function displayProductsInCatalog(){
                 <a href="../../pages/product.html?id=${product.id_productos}" id="product-title-${product.id_productos}" class="product__title">
                     <p class="product__title">${product.nombre}</p>
                 </a>
-                <p class="product__description">${product.descripcion}</p>
+                <p class="product__description">${product.descripcion}</p> <p class="product__category--cat">${product.categorias}</p>
+                <p class="product__info--cat">${product.informacion}</p>
                 <p class="product__price">$${product.precio.toLocaleString()}</p>
             </div>
         </div>
@@ -75,7 +76,6 @@ async function displayProductsInCatalog(){
     })
     filterProduct("Todos");
 }
-
 //CATEGORY - FILTER
 function filterProduct(value) {
   let buttons = document.querySelectorAll(".list__filter");
@@ -89,7 +89,7 @@ function filterProduct(value) {
 
   const products = document.querySelectorAll(".product--cart");
   products.forEach((product) => {
-    if (value === "Todos" || product.querySelector(".product__category").innerText.includes(value)) {
+    if (value === "Todos" || product.querySelector(".product__category--cat").innerText.includes(value)) {
       product.classList.remove("hide");
     } else {
       product.classList.add("hide");
@@ -97,37 +97,50 @@ function filterProduct(value) {
   });
 }
 
-function searchProduct(value){
-  let buttonSearch = document.querySelector("#button__search");
-  let searchInput = document.getElementById("search__input").value.toLowerCase();
-  const products = document.querySelectorAll(".product--cart");
-  buttonSearch.forEach((searchButton)=>{
-    if (value == button.innerText) {
-      button.classList.add("active");
-    } else {
-      button.classList.remove("active");
-    }
-  });
-
-  products.forEach((product) => {
-    if (product.innerText.toLowerCase().includes(searchInput)) {
-      product.classList.remove("hide");
-    } else {
-      product.classList.add("hide");
-    }
-  });
-
-}
 //SEARCH
-  //   document.getElementById("button__search").addEventListener("click", () => {
-  //   let searchInput = document.getElementById("search__input").value.toLowerCase();
-  //   const products = document.querySelectorAll(".product--cart");
-  
-  //   products.forEach((product) => {
-  //     if (product.innerText.toLowerCase().includes(searchInput)) {
-  //       product.classList.remove("hide");
-  //     } else {
-  //       product.classList.add("hide");
-  //     }
-  //   });
-  // });
+document.addEventListener("DOMContentLoaded", async () => {
+  const searchInput = document.getElementById("search__input");
+  const button = document.getElementById("button__search");
+
+  button.addEventListener("click", () => {
+    event.preventDefault();
+    const searchValue = searchInput.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    const products = document.querySelectorAll(".product--cart");
+
+    products.forEach((product) => {
+      const productText = product.innerText.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      const infoCat = product.querySelector(".product__info--cat").innerText.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      if (productText.includes(searchValue) || infoCat.includes(searchValue)) {
+        product.classList.remove("hide");
+      } else {
+        product.classList.add("hide");
+      }
+    });
+  });
+});
+
+//PRICE-ORDER
+  const button = document.querySelector('#button__search');
+  const select = document.querySelector('.order');
+  select.addEventListener('change', () => {
+      const sortBy = select.value;
+      const products = Array.from(document.querySelectorAll('.product--cart'));
+      if (sortBy === 'priceasc') {
+          products.sort((a, b) => {
+              const priceA = parseFloat(a.querySelector('.product__price').textContent.replace('$', ''));
+              const priceB = parseFloat(b.querySelector('.product__price').textContent.replace('$', ''));
+              return priceA - priceB;
+          });
+      } else if (sortBy === 'pricedesc') {
+          products.sort((a, b) => {
+              const priceA = parseFloat(a.querySelector('.product__price').textContent.replace('$', ''));
+              const priceB = parseFloat(b.querySelector('.product__price').textContent.replace('$', ''));
+              return priceB - priceA;
+          });
+      }
+      const padreProductos = document.getElementById("padreProductos");
+      padreProductos.innerHTML = '';
+      products.forEach(product => {
+          padreProductos.appendChild(product);
+      });
+  });
